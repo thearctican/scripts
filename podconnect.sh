129876386891c2b3a91b3130cdc69870c8937427
Matt Wilson <sm.wilson@me.com>
2021-01-26 16:33:06 -0500

    Adding initial version
    V1 of an automated bootstrapping of a shell into your running pods.

---
+++ podconnect.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#This script is intended to bootstrap kubectl and allow for selection of target pods in a sane way. 
+echo "By default, this utility will use the kubeconfig at the location '~/.kube/config'. You can specify an alternative configuration by passing it as an argument for this script."
+echo "Using kubeconfig:"
+
+#Default to standard kubeconfig path if no config file is specified at runtime
+kubeconfigpath="${1:-~/.kube/config}"
+
+#Confirm that the path read from 'read' is correct
+echo $kubeconfigpath
+
+#call the specified path to the config to get pods and filter for polaris pods.
+podlist=`kubectl --kubeconfig "$kubeconfigpath" get pods --field-selector status.phase=Running -o custom-columns=":metadata.name"`
+echo $podlist
+
+#Select target pod and initiate a shell prompt
+select pod in $podlist
+    do  echo You have selected $pod;kubectl --kubeconfig $kubeconfigpath exec -it $pod /bin/bash
+done 
