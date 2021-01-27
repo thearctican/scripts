0f0943e3a6931973846c505ff62de91f6769b915
Matt Wilson <sm.wilson@me.com>
2021-01-27 10:51:15 -0500

    Update podconnect.sh

--- podconnect.sh
+++ podconnect.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 #This script is intended to bootstrap kubectl and allow for selection of target pods in a sane way. 
-echo "By default, this utility will use the kubeconfig at the location '~/.kube/config'. You can specify an alternative configuration by passing it as an argument for this script."
+echo "By default, this utility will use the kubeconfig at in your account's default location. You can specify an alternative configuration by passing the file path as an argument for this script."
 sleep 3
 echo "Using kubeconfig:"
 
 #Default to standard kubeconfig path if no config file is specified at runtime
-kubeconfigpath="${1:-~/.kube/config}"
+echo $HOME
+kubeconfigpath="${1:-$HOME/.kube/config}"
 
 #Confirm that the path read from 'read' is correct
 echo $kubeconfigpath
 
 #call the specified path to the config to get pods and filter for polaris pods.
 podlist=`kubectl --kubeconfig "$kubeconfigpath" get pods --field-selector status.phase=Running -o custom-columns=":metadata.name"`
-nslist=`kubectl --kubeconfig "$kubeconfigpath" get namespaces -o custom-columns=":metadata.name"`
+
 
 #Select target pod and initiate a shell prompt
 if [ `grep -q namespace $kubeconfigpath; echo $?` = 0 ]; then	
@@ -23,7 +24,7 @@ if [ `grep -q namespace $kubeconfigpath; echo $?` = 0 ]; then
 			kubectl --kubeconfig $kubeconfigpath exec -it $pod -- /bin/bash ; exit
 		done
 	else
-		
+		nslist=`kubectl --kubeconfig "$kubeconfigpath" get namespaces -o custom-columns=":metadata.name"`
 		select namespace in $nslist
 				do
 					echo "You have selected $namespace. Please select your pod:"
